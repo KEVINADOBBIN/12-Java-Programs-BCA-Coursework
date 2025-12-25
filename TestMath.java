d93418053aae85c9d82d03bfcbd17009aa651c95
Kevin A Dobbin <94518887+KEVINADOBBIN@users.noreply.github.com>
2025-12-25 13:22:35 +0530

    Add files via upload

---
+++ TestMath.java
@@ -0,0 +1,28 @@
+class TestMath {
+
+    public static void main(String[] args) {
+
+        System.out.println("E:\t\t\t\t" + Math.E);
+        System.out.println("PI:\t\t\t\t" + Math.PI);
+
+        System.out.println("abs(-1234.59):\t\t\t" + Math.abs(-1234.59));
+
+        System.out.println("ceil(9.01):\t\t\t" + Math.ceil(9.01));
+        System.out.println("floor(9.01):\t\t\t" + Math.floor(9.01));
+
+        System.out.println("min(-15, 15):\t\t\t" + Math.min(-15, 15));
+        System.out.println("max(-15, 15):\t\t\t" + Math.max(-15, 15));
+
+        System.out.println("random():\t\t\t" + Math.random());
+
+        System.out.println("round(15.5):\t\t\t" + Math.round(15.5));
+
+        System.out.println("sqrt(4):\t\t\t" + Math.sqrt(4));
+
+        System.out.println("sin(90):\t\t\t" + Math.sin(90));
+        System.out.println("cos(90):\t\t\t" + Math.cos(90));
+        System.out.println("tan(90):\t\t\t" + Math.tan(90));
+
+        System.out.println("pow(2, 2):\t\t\t" + Math.pow(2, 2));
+    }
+}
